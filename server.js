55277c1baa2d223a1f095d0989a3d579919b7149
kuldeepsrathaur <kuldeepsrathaur@users.noreply.github.com>
2017-02-18 20:38:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/ui/article-two.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
+app.get('ui/article-three.html', function (req,res) {
+    res.sendFile(path.join(_dirname, 'ui', 'article-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
