e023920ad31634607a0eaa7adf282c3b81ceac3a
kuldeepsrathaur <kuldeepsrathaur@users.noreply.github.com>
2017-02-18 23:33:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleTwo={
+    title: "Atricle Two",
+    heading: "Article Two",
+    date: "Sep 18 2016",
+    content:         
+     `   <p>
+            This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two.
+            </p>
+            <p>
+            This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two.
+            </p> `
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content= data.content;
+
+var htmlTemplate=
+` <html>
+    <head>
+        <title>
+            $(title);
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+        <div>
+            <h1>$(heading);</h1>
+        </div>
+        <div>
+            $(date);
+        </div>
+        <div>
+            $(content);
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +61,7 @@ app.get('/ui/article-one.html', function (req, res) {
 });
 
 app.get('/ui/article-two.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 
 app.get('/ui/article-three.html', function (req, res) {
