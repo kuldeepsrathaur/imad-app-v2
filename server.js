731dd3ae8b8d90ccd7bce30166ba2668fbea61be
kuldeepsrathaur <kuldeepsrathaur@users.noreply.github.com>
2017-02-18 23:54:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/ui/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two.html', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.send(createTemplate(articleTwo));
 });
 
