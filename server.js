aabada87ef30b58f6afd9a184099e3373b70835c
kuldeepsrathaur <kuldeepsrathaur@users.noreply.github.com>
2017-03-09 21:39:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,16 +85,15 @@ app.get('/', function (req, res) {
 });
 
 var pool= new Pool(config);
-app.get('/test-db',function(req,res){
+app.get('/test-db',function(req, res){
     pool.query("select * from test", function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
         else
         res.send(JSON.stringfy(result));
-    })
-    res.send(err.tostring());
-})
+    });
+   });
 app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename;
   res.send(createTemplate(articles[articlename]));
