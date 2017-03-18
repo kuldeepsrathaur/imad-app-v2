cef80f1e4b1215fb2198dff810a659032acc2af8
kuldeepsrathaur <kuldeepsrathaur@users.noreply.github.com>
2017-03-18 08:03:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,109 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool= require('pg').Pool;
-var config={
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    user: 'kuldeepsrathaur',
-    password: process.env.DB_PASSWORD,
-    database: 'kuldeepsrathaur'
-};
 var app = express();
 app.use(morgan('combined'));
 
-var articles={
-    'article-one' :{ 
-    title: 'Atricle One',
-    heading: 'Article One',
-    date: 'Sep 20 2016',
-    content:         
-     `   <p>
-            This is Article ONe. This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe.This is Article ONe. 
-            </p> `
-},
-    'article-two' : {
-     title: 'Atricle Two',
-    heading: 'Article Two',
-    date: 'Sep 18 2016',
-    content:         
-     `   <p>
-            This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two.
-            </p>
-            <p>
-            This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two. This is Article - two.
-            </p> `
-},   
-    'article-three' : {
-    title: 'Atricle Three',
-    heading: 'Article Three',
-    date: 'Sep 20 2016',
-    content:         
-     `   <p>
-            This is Article - three. This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.This is Article - three.
-            </p> `
-}
-};
-
-function createTemplate(data)
-{
-    var title= data.title;
-    var heading= data.heading;
-    var date= data.date;
-    var content= data.content;
-
-var htmlTemplate=
-` <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-       <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <div>
-            <h3>${heading}</h3>
-        </div>
-        <div>
-            ${date}
-        </div>
-        <div>
-        ${content}
-         </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool= new Pool(config);
-app.get('/test-db', function (req, res){
-    pool.query("select * from test", function(err, result){
-        if(err){
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-        res.send(JSON.stringify(result.rows));
-        }
-    });
-   });
-app.get('/:articlename', function (req, res) {
-    var articlename=req.params.articlename;
-  res.send(createTemplate(articles[articlename]));
-});
-
-app.get('/ui/article-three.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
